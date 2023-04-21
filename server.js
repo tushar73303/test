45c725958b6cee803beaa4a166cee0dd8c3fbff7
tushar73303 <tushar73303@gmail.com>
2023-04-21 16:50:05 -0400

    changes

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-    // handle form submission
-    $('#loginForm').submit(function(event) {
-      event.preventDefault(); 
-      var username = $('#username').val(); 
-      var password = $('#password').val(); 
-      if (username.trim() !== '' && password.trim() !== '') {
-        
-        var passwordHash = sha256(password); 
-        window.location.href = 'login-success.html?username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(passwordHash);
-      }
-    });
+  // handle form submission
+  $('#loginForm').submit(function(event) {
+    event.preventDefault(); // prevent default form submission
+    var username = $('#username').val(); // get username value
+
+    var password = $('#password').val(); // get password value
+    if (username.trim() !== '' && password.trim() !== '') {
+      
+      // redirect to login success page with username and password hash as URL parameters
+      var passwordHash = sha256(password); 
+      window.location.href = 'login-success.html?username=' + encodeURIComponent(username) + '&password=' + encodeURIComponent(passwordHash);
+    }
   });
-  
-  
-  
+});
